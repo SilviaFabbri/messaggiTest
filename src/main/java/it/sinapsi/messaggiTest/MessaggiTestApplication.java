5d138f00303627eb7a98b024414381ef120e49dd
silvia <laje@gmail.com>
2023-05-12 09:19:12 +0200

    Initial commit

---
+++ src/main/java/it/sinapsi/messaggiTest/MessaggiTestApplication.java
@@ -0,0 +1,13 @@
+package it.sinapsi.messaggiTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessaggiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessaggiTestApplication.class, args);
+	}
+
+}
